33dfaa1aa76cbfceed60c06c6e1c6f22a417b8a6
Paul Logston <code@logston.me>
2014-08-26 12:22:25 -0400

    Update setup.py for PyPI upload.

--- setup.py
+++ setup.py
@@ -11,14 +11,16 @@ setup(
     long_description=open("README.rst").read(),
     author='Paul Logston',
     author_email='code@logston.me',
-
+    url = 'https://github.com/logston/cunidecode',
+    download_url = 'https://github.com/logston/cunidecode/tarball/0.0.1',
     ext_modules=[Extension('cunidecode', ['cunidecode.c'])],
-
     provides = [ 'cunidecode' ],
     test_suite='tests',
+    keywords = ['unicode', 'c', 'unidecode'],
     classifiers = [
     "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
 	"Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
 	"Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: Implementation :: CPython",
 	"Topic :: Text Processing",
